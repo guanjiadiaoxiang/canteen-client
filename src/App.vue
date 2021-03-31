35816205736f4c875c0c82ba2e1f0f207558492d
guanjiadiaoxiang <1399181232@qq.com>
2021-03-31 18:22:52 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+       <span><i class="iconfont iconguanli"></i></span>
+       <span><i></i></span>
+       <span><i></i></span>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+$test: red;
+.main{
+    width: 300px;
+    height: 200px;
+    background: $test;
+}
+</style>
